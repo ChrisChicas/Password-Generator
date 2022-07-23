1190748852296016633f016da1d6ebb0ae6729da
Chris Chicas <christianchicas7@gmail.com>
2022-07-23 14:58:01 -0400

    Finished styling + added comments within code. WIP Readme

--- README.md
+++ README.md
@@ -1,70 +1,18 @@
-# Getting Started with Create React App
+# Password Generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+A simple random password generator built with React and styled with Bootstrap and a bit of Sass. 
 
-## Available Scripts
+## Features
+- Select different password difficulties which determine their length: Easy (8), Medium (12) & Hard (16)! (Password length displayed next to the difficulty)
+- Password lengths not to your liking? Select custom length to determine your own password length! (Custom length must be above or at 6 characters)
+- Select whether or not to include special characters in your password! (Special characters include: ~!@#$%^&*()-_+={[]}|';:/?,.<>)
+- Once all of your preferences are selected, simply click the "Generate!" button to generate your password!
+- Not a fan of the password generated for you? Passwords are generated randomly and displayed dynamically, so you can click the "Generate!" button as many times until you get a password you are satisfied with!
 
-In the project directory, you can run:
+## Application Link
+- Hosted on
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Local Setup
+- If you'd rather run the application locally, simply git clone this repo down into your local machine using a terminal and cd into the folder.
+- Afterwards, open the code editor of your choice within the folder. In the code editor's terminal, run `npm install` to install all necessary dependencies.
+- Once that is complete, run `npm run start` to start the application!
--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 function App() {
   const [passwordPreferences, setPasswordPreferences] = useState({
     difficulty: "Easy",
-    customLen: 1,
+    customLen: 6,
     special: false
-  })
-  const [password, setPassword] = useState("")
-  const nums = Array.from(Array(10).keys())
-  const lowerChars = "abcdefghijklmnopqrstuvwxyz".split("")
-  const upperChars = lowerChars.map(char => {return char.toUpperCase()})
-  const specialChars = "~!@#$%^&*()-_+={[]}|';:/?,.<>".split("")
-  const baseChars = [...nums, ...lowerChars, ...upperChars]
-  const baseSpChars = [...baseChars, ...specialChars]
+  }) // React useState variable that holds information which is plugged into the Generate function below
+  const [password, setPassword] = useState("") // React useState variable that holds generated password, used in the Generate function
+  const nums = Array.from(Array(10).keys()) // Creates an array of numbers from 0-9
+  const lowerChars = "abcdefghijklmnopqrstuvwxyz".split("") // Splits string into array of lowercase letters a-z
+  const upperChars = lowerChars.map(char => {return char.toUpperCase()}) // Takes array of lowercase letters and returns array of uppercase letters
+  const specialChars = "~!@#$%^&*()-_+={[]}|';:/?,.<>".split("") // Splits string into array of special characters
+  const baseChars = [...nums, ...lowerChars, ...upperChars] // Main array of base characters for passwords (Does not include special characters)
+  const baseSpChars = [...baseChars, ...specialChars] // Array of base characters + special characters
 
-  const Generate = (e, difficulty, customLen, special) => {
-      e.preventDefault()
-      let curLen = 0
-      let newPass = ""
+  const Generate = (e, difficulty, customLen, special) => { // Password generator function. Returns passwords based on if they include special characters + length of password selected
+      e.preventDefault() // Prevents form submit
+      let curLen = 0 // Current generated password length
+      let newPass = "" // Empty string which will contain the new password being generated
       if (special === false){
           if (difficulty === "Easy"){
               while (curLen < 8){
@@ -26,28 +26,24 @@ function App() {
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else if (difficulty === "Medium"){
               while (curLen < 12){
                   newPass = newPass + baseChars[Math.floor(Math.random() * baseChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else if (difficulty === "Hard"){
               while (curLen < 16){
                   newPass = newPass + baseChars[Math.floor(Math.random() * baseChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else{
               while (curLen < customLen){
                   newPass = newPass + baseChars[Math.floor(Math.random() * baseChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           }
       } else {
           if (difficulty === "Easy"){
@@ -56,33 +52,29 @@ function App() {
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else if (difficulty === "Medium"){
               while (curLen < 12){
                   newPass = newPass + baseSpChars[Math.floor(Math.random() * baseSpChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else if (difficulty === "Hard"){
               while (curLen < 16){
                   newPass = newPass + baseSpChars[Math.floor(Math.random() * baseSpChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           } else{
               while (curLen < customLen){
                   newPass = newPass + baseSpChars[Math.floor(Math.random() * baseSpChars.length)]
                   curLen++
               }
               setPassword(newPass)
-              return password
           }
       }
   }
   return (
-    <main className="container-fluid">
+    <main className="container-fluid main">
         <h1><u>Password Generator</u></h1>
         <form onSubmit={e => {Generate(e, passwordPreferences.difficulty, passwordPreferences.customLen, passwordPreferences.special)}}>
             <div className="form-check">
@@ -98,24 +90,27 @@ function App() {
                 <input className="form-check-input" type="radio" name="difficulty" id="hard" value="Hard" onClick={e => {setPasswordPreferences({...passwordPreferences, difficulty: e.target.value})}}/>
             </div>
             <div className="form-check">
-                <label className="form-check-label" htmlFor="custom">Custom</label>
+                <label className="form-check-label" htmlFor="custom">Custom Length:</label>
                 <input className="form-check-input" type="radio" name="difficulty" id="custom" value="Custom" onClick={e => {setPasswordPreferences({...passwordPreferences, difficulty: e.target.value})}}/>
-                <input className="form-control" type="number" defaultValue="1" min="1" onChange={e => {setPasswordPreferences({...passwordPreferences, customLen: e.target.value})}}/>
+                <input className="form-control" type="number" defaultValue="6" min="6" onChange={e => {setPasswordPreferences({...passwordPreferences, customLen: e.target.value})}}/>
             </div>
-            <div className="form-check">
+            <div className="form-check special">
                 <label className="form-check-label" htmlFor="specialF">Without Special Characters</label>
                 <input className="form-check-input" type="radio" name="special" id="specialF" onClick={() => {setPasswordPreferences({...passwordPreferences, special: false})}} defaultChecked/>
-                <br/>
+            </div>
+            <div className="form-check special">
                 <label className="form-check-label" htmlFor="specialT">With Special Characters</label>
                 <input className="form-check-input" type="radio" name="special" id="specialT" onClick={() => {setPasswordPreferences({...passwordPreferences, special: true})}}/>
             </div>
-            <button className="btn btn-success" type="submit">Generate!</button>    
+            <br/>
+            <button className="btn btn-light" type="submit">Generate!</button>    
         </form>
         { password ? 
-        <div className="container-fluid">
-            <h2>Your password: {password}</h2>
+        <div className="container-fluid password">
+            <h2><u>Your password:</u><br/> {password}</h2>
         </div> :
-        null }
+        null } 
+        {/* Above div will only appear after a password has been generated */}
     </main>
   );
 }
